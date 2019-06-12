ebd386595fb8545c3349e7ede26c478f1a03fbf2
yohei.katayama <yohei.katayama@CS-513.CSRN.local>
2019-06-12 15:24:39 +0900

    update IndexServlet

--- WakabaKakeibo/src/web/IndexServlet.java
+++ WakabaKakeibo/src/web/IndexServlet.java
@@ -36,7 +36,7 @@ public class IndexServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		//ユーザの好感度の取得
+				//ユーザの好感度の取得
 				//ここではユーザの好感度は0－2を考えている
 				//とりあえず0を代入する
 				int userFeelingLevel = 0;
@@ -54,15 +54,7 @@ public class IndexServlet extends HttpServlet {
 				//Message用のListの生成
 				List<MessageBean> mList = new ArrayList();
 
-				//朝、昼、夜によって、messageIdの値を変える
-				//朝1－3、昼4－6、夜7－9
-				if(6 < ld.getHour() && ld.getHour() < 12) {
-					messageId += 0;
-				}else if(12 < ld.getHour() && ld.getHour() < 18) {
-					messageId += 3;
-				}else{
-					messageId += 6;
-				};
+
 
 				//サービスを取得
 				UsersService service = new UsersService();
@@ -71,18 +63,9 @@ public class IndexServlet extends HttpServlet {
 				//Dtoからユーザの誕生日を取り出す
 				LocalDateTime userBirthDay = LocalDateTime.now();
 
-				//今日がユーザの誕生日ならば誕生日コメントを取得
-				if(userBirthDay.getDayOfMonth() == ld.getDayOfMonth() &&
-						userBirthDay.getMonth() == ld.getMonth()) {
 
-					//誕生日メッセージのIDが10－12のため10を足す
-					MessageBean beanBirth = service.findById(userFeelingLevel + 10);
-					mList.add(beanBirth);
-				}
-
-				//挨拶メッセージの取得
-				MessageBean beanGreeting = service.findById(messageId);
-				mList.add(beanGreeting);
+				mList.add(getGreeting(service, ld, messageId));
+				mList.add(getBirth(service, ld, userBirthDay, userFeelingLevel));
 
 				//MessageBeanのListをMessageListBeanへset
 				mListBean.setmBeanList(mList);
@@ -99,6 +82,42 @@ public class IndexServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
+	}
+
+	//挨拶の取得
+	public MessageBean getGreeting(UsersService service, LocalDateTime ld, int messageId) {
+
+		//朝、昼、夜によって、messageIdの値を変える
+		//朝1－3、昼4－6、夜7－9
+		if(6 < ld.getHour() && ld.getHour() < 12) {
+			messageId += 0;
+		}else if(12 < ld.getHour() && ld.getHour() < 18) {
+			messageId += 3;
+		}else{
+			messageId += 6;
+		};
+
+		//挨拶メッセージの取得
+		MessageBean beanGreeting = service.findById(messageId);
+
+		return beanGreeting;
+
+	}
+
+	//誕生日メッセージの取得
+	public MessageBean getBirth(UsersService service, LocalDateTime ld, LocalDateTime userBirthDay, int userFeelingLevel) {
+
+		MessageBean beanBirth = null;
+
+		//今日がユーザの誕生日ならば誕生日コメントを取得
+		if(userBirthDay.getDayOfMonth() == ld.getDayOfMonth() &&
+				userBirthDay.getMonth() == ld.getMonth()) {
+
+			//誕生日メッセージのIDが10－12のため10を足す
+			beanBirth = service.findById(userFeelingLevel + 10);
+
+		}
+		return beanBirth;
 	}
 
 }
